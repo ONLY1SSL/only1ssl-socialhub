ff8f1b028b1fb2823fd2b821aa0f185d6c2f3848
ONLY1SSL <ibrahimhammed974@gmail.com>
2025-07-15 23:05:07 +0100

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,12 @@
 // routes/productRoutes.js
 const express = require('express');
-const router = express.Router();
-const Product = require('../models/Product');
+const router = express.router();
+const product = require('../models/product');
 
 // Get all available products
 router.get('/', async (req, res) => {
   try {
-    const products = await Product.find({ available: true });
+    const products = await product.find({ available: true });
     res.json(products);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch products' });
