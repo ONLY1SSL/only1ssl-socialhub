3b5eac896a567738adbc4eba24311deb85d91dba
ONLY1SSL <ibrahimhammed974@gmail.com>
2025-07-15 17:06:02 +0100

    Create model.products.js

---
+++ model.products.js
@@ -0,0 +1,11 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  platform: { type: String, required: true }, // e.g. Instagram, Facebook
+  price: { type: Number, required: true }, // in Naira
+  available: { type: Boolean, default: true }
+});
+
+module.exports = mongoose.model('Product', productSchema);
